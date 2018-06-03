2bbd91189cbfd6824a767f0cc87a55b348227399
Mark Pothier <mark.d.pothier@gmail.com>
2018-06-03 02:49:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ function viewCart() {
   else if (cart.length === 1) {
     return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`;
   }
+  debugger;
   else if (cart.length > 1) {
     var strStart = [];
     for (var i = 0; i < (cart.length)-1; i++) {
