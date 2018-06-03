25fb83b80dc151ff5d17d69d1e4354ae06e6bfcd
Mark Pothier <mark.d.pothier@gmail.com>
2018-06-03 03:37:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -56,9 +56,9 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   if (cardNumber === undefined) {
-    return "Sorry, we don't have a credit card on file for you."
+    return "Sorry, we don't have a credit card on file for you.";
   }
   else {
-    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+    var str = `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
   }
 }
