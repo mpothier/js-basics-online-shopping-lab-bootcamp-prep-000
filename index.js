71e69821a994684d15a076b08e575e0d9be5f5cf
Mark Pothier <mark.d.pothier@gmail.com>
2018-06-03 02:46:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,10 @@ function addToCart(item) {
   return `${item} has been added to your cart.`;
 }
 
+var cart = [
+  {itemName:"bananas"
+  ]
+
 function viewCart() {
   if (cart.length === 0) {
     return "Your shopping cart is empty.";
