6c4aedb498747c51949063e386ca951c2a326b3d
Mark Pothier <mark.d.pothier@gmail.com>
2018-06-03 03:21:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ function total() {
 function removeFromCart(item) {
   for (var i=0; i < cart.length; i++) {
     if (cart[i].itemName === item) {
-      cart.splice
+      cart.splice(i,1)
       return cart 
     }
   }
