72a132dce8e6297fd5431ba6a0594a39cc4bdf49
Mark Pothier <mark.d.pothier@gmail.com>
2018-06-03 02:50:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ function viewCart() {
     return "Your shopping cart is empty.";
   }
   else if (cart.length === 1) {
+    console.log(cart[0].itemName)
     return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`;
   }
   debugger;
